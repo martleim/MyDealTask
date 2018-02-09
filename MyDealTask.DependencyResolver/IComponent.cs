337372422073d49244b9b3eada46fd1031c5172c
martleim <martleim@gmail.com>
2018-02-09 15:10:25 +1100

    fix Icomponent

--- MyDealTask.DependencyResolver/IComponent.cs
+++ MyDealTask.DependencyResolver/IComponent.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace MyDealTask.DependencyResolver
+namespace GenericAirways.DependencyResolver
 {
     public interface IComponent
     {
-        void SetUp(IRegisterComponent registerComponent);
+        void SetUp(IServiceCollection services);
     }
 }
