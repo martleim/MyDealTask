14c0fa8400f39290aeb76c3d49fcebe9d7074dda
martleim <martleim@gmail.com>
2018-02-09 15:10:52 +1100

    fix I register compoennt

--- MyDealTask.DependencyResolver/IRegisterComponent.cs
+++ MyDealTask.DependencyResolver/IRegisterComponent.cs
@@ -1,8 +1,8 @@
-﻿namespace MyDealTask.DependencyResolver
+namespace GenericAirways.DependencyResolver
 {
     public interface IRegisterComponent
     {
-        void RegisterType<TFrom, TTo>(bool withInterception = false) where TTo : TFrom;
-        void RegisterTypeWithControlledLifeTime<TFrom, TTo>(bool withInterception = false) where TTo : TFrom;
+        void RegisterType<TFrom, TTo>(bool withInterception = false) where TTo : class, TFrom where TFrom : class;
+        void RegisterTypeWithControlledLifeTime<TFrom, TTo>(bool withInterception = false) where TTo : class, TFrom where TFrom : class;
     }
 }
